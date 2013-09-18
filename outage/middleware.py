95fd6adf0fefc754c6805bcb6b2b329bc5a3b73b
Todd Graves <todd@snoball.com>
2013-09-18 10:43:46 -0500

    Django tests sometimes override the ROOT_URLCONF, so I'm adding ability to skip the middleware when testing.

--- outage/middleware.py
+++ outage/middleware.py
@@ -1,10 +1,23 @@
+import sys
+
 from django.core.urlresolvers import reverse
-from outage.models import ShutDown
 from django.shortcuts import redirect
+
+from outage.models import ShutDown
+
+_using_manage = True in ['manage.py' in arg for arg in sys.argv]
+
+TESTING = ((_using_manage and 'test' in sys.argv) or ('nosetests' in sys.argv))
 
 
 class OutageMiddleware(object):
     def process_request(self, request):
+
+        # Django tests may set their own ROOT_URLCONF, in which case we may not
+        # be able to resolve 'outage', so we'll just return None unless testing
+        # this app intentionally.
+        if TESTING and 'outage' not in sys.argv:
+            return None
 
         if (request.path == reverse('outage') or
             u'static' in request.path.split('/')
@@ -15,3 +28,4 @@ class OutageMiddleware(object):
             return redirect('outage')
 
         return None
+
